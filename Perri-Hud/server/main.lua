070bed9f496dbc2e0f057d7a3fe263712d3cc576
TrolerTuber <94766847+TrolerTuber@users.noreply.github.com>
2024-09-08 18:29:40 +0200

    Update main.lua

--- Perri-Hud/server/main.lua
+++ Perri-Hud/server/main.lua
@@ -1,61 +1,60 @@
 if Config.EnableOnlinejobs then
-RegisterNetEvent('Perri_OnlineJobs:server:load', function()
-    local playerId = source
-    local xPlayer = ESX.GetPlayerFromId(playerId)
+    RegisterNetEvent('Perri_OnlineJobs:server:load', function()
+        local playerId = source
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        
         TriggerClientEvent('Perri_OnlineJobs:client:load', playerId, Config.Jobs)
-    if GetJobFromConfig(xPlayer.job.name) then
-        Config.Jobs[GetJobIndexFromConfig(xPlayer.job.name)].count = GetCountFromJob(xPlayer.job.name)
+        
+        if GetJobFromConfig(xPlayer.job.name) then
+            Config.Jobs[GetJobIndexFromConfig(xPlayer.job.name)].count = GetCountFromJob(xPlayer.job.name)
+            TriggerClientEvent('Perri_OnlineJobs:client:sync', -1, Config.Jobs)
+        end
+    end)
 
+    RegisterNetEvent('esx:playerDropped', function(playerId, reason)
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+
+        if xPlayer and GetJobFromConfig(xPlayer.job.name) then
+            Config.Jobs[GetJobIndexFromConfig(xPlayer.job.name)].count = GetCountFromJob(xPlayer.job.name)
+            TriggerClientEvent('Perri_OnlineJobs:client:sync', -1, Config.Jobs)
+        end
+    end)
+
+    RegisterNetEvent('esx:setJob', function(playerId, job, lastJob)
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        
+        if GetJobFromConfig(job.name) then
+            Config.Jobs[GetJobIndexFromConfig(job.name)].count = GetCountFromJob(job.name)
+        end
+        
+        if GetJobFromConfig(lastJob.name) then
+            Config.Jobs[GetJobIndexFromConfig(lastJob.name)].count = GetCountFromJob(lastJob.name)
+        end
 
         TriggerClientEvent('Perri_OnlineJobs:client:sync', -1, Config.Jobs)
-    end
-end)
+    end)
 
-RegisterNetEvent('esx:playerDropped', function(playerId, reason)
-    local xPlayer = ESX.GetPlayerFromId(playerId)
-
-    if xPlayer and GetJobFromConfig(xPlayer.job.name) then
-        Config.Jobs[GetJobIndexFromConfig(xPlayer.job.name)].count = GetCountFromJob(xPlayer.job.name)
-
-        TriggerClientEvent('Perri_OnlineJobs:client:sync', -1, Config.Jobs)
-    end
-end)
-
-RegisterNetEvent('esx:setJob', function(playerId, job, lastJob)
-    local xPlayer = ESX.GetPlayerFromId(playerId)
-    if GetJobFromConfig(job.name) then
-        Config.Jobs[GetJobIndexFromConfig(job.name)].count = GetCountFromJob(job.name)
-    end
-    if GetJobFromConfig(lastJob.name) then
-        Config.Jobs[GetJobIndexFromConfig(lastJob.name)].count = GetCountFromJob(lastJob.name)
-    end
-
-    TriggerClientEvent('Perri_OnlineJobs:client:sync', -1, Config.Jobs)
-end)
-
-
-function GetJobFromConfig(jobName)
-    for k,v in pairs(Config.Jobs) do
-        if v.name == jobName then
-            return v
+    function GetJobFromConfig(jobName)
+        for k,v in pairs(Config.Jobs) do
+            if v.name == jobName then
+                return v
+            end
         end
+
+        return false
     end
 
-    return false
-end
-
-function GetJobIndexFromConfig(jobName)
-    for k,v in pairs(Config.Jobs) do
-        if v.name == jobName then
-            return k
+    function GetJobIndexFromConfig(jobName)
+        for k,v in pairs(Config.Jobs) do
+            if v.name == jobName then
+                return k
+            end
         end
+
+        return false
     end
 
-    return false
-end
-
-function GetCountFromJob(jobName)
-    return #ESX.GetExtendedPlayers('job', jobName)
-end
-
+    function GetCountFromJob(jobName)
+        return #ESX.GetExtendedPlayers('job', jobName)
+    end
 end
